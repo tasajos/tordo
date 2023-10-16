ccbc76d607a0b634fb41bebd120ecf0b053df06d
Carlos Andres <tasajos@gmail.com>
2023-10-16 13:01:43 -0400

    update routing

--- Tordo/src/app/app-routing.module.ts
+++ Tordo/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PrincipalComponent } from './Components/principal/principal.component';
+import { PagosComponent } from './Components/pagos/pagos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+{path: '', redirectTo:'principal', pathMatch:'full'},
+{path: 'principal',component:PrincipalComponent},
+{path: 'pagos',component:PagosComponent},
+{path: '**', redirectTo:'principal', pathMatch:'full'}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
