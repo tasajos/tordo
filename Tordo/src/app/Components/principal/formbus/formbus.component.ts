57933819cf2b7ad582194d49d971b0112e05ddc7
tasajos <tasajos@gmail.com>
2023-10-20 22:28:15 -0400

    update formulario busqueda

--- Tordo/src/app/Components/principal/formbus/formbus.component.ts
+++ Tordo/src/app/Components/principal/formbus/formbus.component.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SflotaService } from 'src/app/Services/sflota.service';
+import { registrarflotaInter } from 'src/app/Interfaz/flota';
+
 
 @Component({
   selector: 'app-formbus',
   templateUrl: './formbus.component.html',
   styleUrls: ['./formbus.component.css']
 })
-export class FormbusComponent {
+export class FormbusComponent   implements OnInit {
   mostrarTabla: boolean = false;
+  registrosFlota: registrarflotaInter[] = [];
 
+  constructor(private verFlota: SflotaService) {}
 
+  ngOnInit(): void {
+    this.verFlota.getflota().subscribe(data => {
+      this.registrosFlota = data; // Almacena los datos en la variable
+    });
+  }
   mostrarResultado() {
     this.mostrarTabla = true;
   }
