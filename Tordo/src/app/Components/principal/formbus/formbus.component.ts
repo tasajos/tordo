499c745edb4f4b27371f18877e642937c6328aee
tasajos <tasajos@gmail.com>
2023-10-22 08:25:50 -0400

    formulario update

--- Tordo/src/app/Components/principal/formbus/formbus.component.ts
+++ Tordo/src/app/Components/principal/formbus/formbus.component.ts
@@ -1,40 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef,AfterViewInit } from '@angular/core';
 import { SflotaService } from 'src/app/Services/sflota.service';
 import { registrarflotaInter } from 'src/app/Interfaz/flota';
+import { Modal } from 'bootstrap'; // <-- Importa Bootstrap
 
 @Component({
   selector: 'app-formbus',
   templateUrl: './formbus.component.html',
   styleUrls: ['./formbus.component.css']
 })
-export class FormbusComponent implements OnInit {
+export class FormbusComponent implements OnInit, AfterViewInit {
   mostrarTabla: boolean = false;
   registrosFlota: registrarflotaInter[] = [];
   origen: string = '';
   destino: string = '';
-  mostrarModal: boolean = false;
+  mostrarModal: boolean = false ;
 
+  @ViewChild('modalNoResultados') modalNoResultados!: ElementRef;
+
+  private bsModal!: Modal; // Instancia del modal de Bootstrap
   constructor(private verFlota: SflotaService, private buscarFlotaService: SflotaService) {}
+
+
 
   ngOnInit(): void {
     this.verFlota.getflota().subscribe(data => {
       this.registrosFlota = data;
     });
+    
+  }
+
+  ngAfterViewInit(): void { // <-- Añade esta función
+    this.bsModal = new Modal(this.modalNoResultados.nativeElement);
   }
 
   buscarPorOrigenYDestino() {
     if (this.origen && this.destino) {
       this.buscarFlotaService.buscarFlota(this.origen, this.destino)
         .subscribe(data => {
-          // Verifica si la respuesta tiene la propiedad "Mensaje"
+          // Verifica si la respuesta tiene la propiedad "mensaje"
           if (data.mensaje) {
-            // Aquí puedes mostrar el modal o una alerta
-            alert(data.mensaje);
-            this.mostrarModal = true;
+            // Muestra el modal de Bootstrap
+            this.bsModal.show();
           } else {
             this.registrosFlota = data;
             this.mostrarTabla = data.length > 0;
-            this.mostrarModal = false; 
           }
         }, error => {
           alert('Error al realizar la búsqueda.');
@@ -43,6 +52,4 @@ export class FormbusComponent implements OnInit {
       alert('Por favor, seleccione un origen y un destino.');
     }
   }
-  
-  
 }
